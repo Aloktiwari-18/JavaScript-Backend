21dc81f09743a804a2dc5f736c5247b32f95bee1
Alok <aloktiwari1095@gmail.com>
2025-05-08 00:36:19 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+const app = express();
+
 
 dotenv.config({ 
     path: "./env" 
